d14a489266fb495350f4a057402282fb34a0c7c8
YJL <1797123777@qq.com>
2018-06-14 10:37:25 +0800

    加入显示天气信息的功能。

---
+++ app/src/main/java/com/example/iweather/gson/AQI.java
@@ -0,0 +1,11 @@
+package com.example.iweather.gson;
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity {
+        public String aqi;
+        public String pm25;
+    }
+}
